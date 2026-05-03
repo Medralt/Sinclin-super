c2f3a913bd7934142a1346f877a89da5ac717569
Dr Marino <marinomendes@hotmail.com>
2026-05-03 15:28:51 -0300

    sinclin: fix render path + runtime

--- staging/api/server.js
+++ staging/api/server.js
@@ -2,8 +2,9 @@ const express=require('express');
 const app=express();
 app.use(express.json());
 
-const d=require('C:/sinclin/core/src/sioc/resolve/decision.engine.js');
+const d=require("../../core/src/sioc/resolve/decision.engine.js");
 
 app.post('/chat',(req,res)=>res.json(d.run(req.body)));
 
 app.listen(3001,()=>console.log('API ON 3001'));
+
